f90e710659f5433ce5db4e79510311dc522febc6
maxim <max_mish@inbox.ru>
2024-01-31 17:11:27 +0300

    -

---
+++ board_games/poll/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from .models import Choice, Question
+
+# Register your models here.
+admin.site.register(Choice)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date", "picture_image"]
+    date_hierarchy = "pub_date"
+
+    def picture_image(self, obj):
+        return mark_safe(f'<img src="{obj.picture or ""}" width="150" height="150" /> ')

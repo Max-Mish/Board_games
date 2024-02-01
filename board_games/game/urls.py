7a7725a0ede4626cf96d698b648208892dbe1467
iushishko.adm <iushishko@sitronics.com>
2024-02-01 17:11:36 +0300

    add booking

--- board_games/game/urls.py
+++ board_games/game/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import GamesViewSet
+from .views import GamesViewSet, GameBookViewSet
 
 router = DefaultRouter()
 router.register(r"games", GamesViewSet, basename="game")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path('booking/', GameBookViewSet.as_view())
 ]
